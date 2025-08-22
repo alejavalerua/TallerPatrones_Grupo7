6387e035de5504ea98ca4e5d9187bb7ac365d62b
alejavalerua <alejandrarua@uninorte.edu.co>
2025-08-21 22:59:59 -0500

    Just Front dev

--- backend/cs-backend/dist/src/tickets/dto/create-ticket.dto.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class CreateTicketDto {
-    title: string;
-    description: string;
-    status?: bigint;
-    priority?: bigint;
-    customer_id?: bigint;
-    assignee_id?: bigint;
-}
--- backend/cs-backend/dist/src/tickets/dto/update-ticket.dto.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class UpdateTicketDto {
-    title?: string;
-    description?: string;
-    status?: bigint;
-    priority?: bigint;
-    customer_id?: bigint;
-    assignee_id?: bigint;
-}
